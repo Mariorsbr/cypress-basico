33c12568a2484e14c4db2addd056f0537191d59b
Mario Victor <mario.rs1996@gmail.com>
2023-11-05 17:00:45 -0300

    (feat) - Solve lesson 02

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -1,10 +1,49 @@
 /// <reference types="Cypress" />
 
 describe('Central de Atendimento ao Cliente TAT', () => {
-  it('verifica o titulo da aplicacao', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
+
+  it('verifica o titulo da aplicacao', () => {
+    // cy.visit('./src/index.html')
     cy.title()
       .should('eq', 'Central de Atendimento ao Cliente TAT')
   })
+
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    // cy.visit('./src/index.html')
+    cy.get('[id="firstName"]')
+      .should('be.visible')
+      .type('nome')
+      .should('have.value', 'nome')
+
+    cy.get('[id = "lastName"]')
+      .should('be.visible')
+      .type('sobrenome')
+      .should('have.value', 'sobrenome')
+
+    cy.get('[id = "email"]')
+      .should('be.visible')
+      .type('teste@teste.com')
+      .should('have.value', 'teste@teste.com')
+
+    cy.get('[id = "open-text-area"]')
+      .should('be.visible')
+      .type('campo obrigatorio')
+      .should('have.value', 'campo obrigatorio')
+
+    cy.get('[class = "success"]')
+      .should('not.be.visible')
+
+    cy.get('[class="button"]')
+      .should('have.text', 'Enviar')
+      .click()
+
+    cy.get('[class = "success"]')
+      .should('be.visible')
+  })
+
+
 
 })
